27d6e7cf8ddfbda80f5c5e3fde500736752f89c7
Gig28 <128386775+Gig28@users.noreply.github.com>
2023-12-01 20:59:39 +0300

    Pizza Recipes task 2

---
+++ app/src/main/java/com/example/pizza/Utils.java
@@ -0,0 +1,92 @@
+package com.example.pizza;
+
+public class Utils {
+    public static final String PIZZA_1_TITLE = "Vikaroni";
+    public static final String PIZZA_1_DESCRIPTION = "Experience the taste of the most delicious pizza in the world. Your tastebuds will be grateful!";
+    public static final String PIZZA_1_RECIPE = "Preheat the oven to 220C." +
+            "\n" +
+            "Prepare the necessary ingredients." +
+            "\n" +
+            "Bake the pizza in the oven until it turns crispy and golden brown." +
+            "\n" +
+            "Enjoy the mouthwatering Vikaroni pizza!";
+
+    public static final String PIZZA_2_TITLE = "LisaBBQ";
+    public static final String PIZZA_2_DESCRIPTION = "Indulge in the unique combination of flavors with LisaBBQ pizza. A treat not recommended for cat lovers!";
+    public static final String PIZZA_2_RECIPE = "Preheat the oven to 220C." +
+            "\n" +
+            "Prepare Lisa The Cat by marinating it with delicious barbecue sauce." +
+            "\n" +
+            "Bake the pizza in the oven until it turns crispy and golden brown." +
+            "\n" +
+            "Savor the flavors of LisaBBQ pizza and enjoy!";
+
+    public static final String PIZZA_3_TITLE = "Regular pizza";
+    public static final String PIZZA_3_DESCRIPTION = "Pizza is an open pie with a dough base, topped with various ingredients, primarily melted cheese." +
+            "\n" +
+            "It is one of the most popular dishes in the world, an Italian national dish. To prepare a delicious pizza, you need a good pizza dough recipe and pizza topping recipe." +
+            "\n" +
+            "There are over five hundred pizza recipes on our website, with many pizza recipes accompanied by photos.";
+    public static final String PIZZA_3_RECIPE = "All the ingredients are calculated for 2 pizzas.\n" +
+            "The pizza dough is prepared according to Jamie Oliver's recipe (link to the recipe is provided in the ingredient list).\n" +
+            "I use homemade tomato sauce with a touch of sweetness." +
+            "\n" +
+            "Place one portion of the dough on a parchment-lined baking sheet greased with a little oil, and spread the dough into a circle with a diameter of 30 cm." +
+            "\n" +
+            "\n" +
+            "Spread the tomato sauce, leaving the edges untouched." +
+            "\n" +
+            "Sprinkle with dried Italian herbs.\n" +
+            "Grate some cheese on top." +
+            "\n" +
+            "I'm tired of writing the recipe, it's getting too long." +
+            "\n" +
+            "Place the pizza in an oven preheated to 190 degrees Celsius and bake for 15-20 minutes, depending on the oven." +
+            "\n" +
+            "Enjoy!";
+
+    public static final String PIZZA_4_TITLE = "Sweet Pizza";
+    public static final String PIZZA_4_DESCRIPTION = "To cook this pizza, you need precision and a touch from Down Under.";
+    public static final String PIZZA_4_RECIPE = "Preheat the oven to 220C." +
+            "\n" +
+            "Gather all the ingredients." +
+            "\n" +
+            "Bake the pizza in the oven until it turns crispy and golden brown." +
+            "\n" +
+            "Eat and recycle it for a guilt-free indulgence." +
+            "\n" +
+            "Savor the flavors of Sweet Pizza and enjoy!";
+
+    public static final String PIZZA_5_TITLE = "Mario!";
+    public static final String PIZZA_5_DESCRIPTION = "You'll love the adventurous flavors of Mario! pizza. It's a one-of-a-kind experience!";
+    public static final String PIZZA_5_RECIPE = "It's a me, Mario!" +
+            "\n" +
+            "Bake the pizza in the oven until it turns crispy and golden brown." +
+            "\n" +
+            "Savor the flavors of Mario! pizza and enjoy!";
+
+    public static final String PIZZA_6_TITLE = "Wild pizza";
+    public static final String PIZZA_6_DESCRIPTION = "Experience the untamed flavors of nature with Wild pizza. Your tastebuds will be grateful!";
+    public static final String PIZZA_6_RECIPE = "Preheat the oven to 220C." +
+            "\n" +
+            "Watch as Pizza peacefully lives with its family in the wild." +
+            "\n" +
+            "Slaughter and collect the remains of Pizza and its family without remorse." +
+            "\n" +
+            "Bake the remains in the oven until they turn crispy and golden brown." +
+            "\n" +
+            "Savor the wild flavors of Wild pizza and enjoy!";
+    public static final String PIZZA_7_TITLE = "Microwaved Regular Pizza";
+    public static final String PIZZA_7_DESCRIPTION = "Experience the convenience of a regular pizza cooked in the microwave.";
+    public static final String PIZZA_7_RECIPE = "Simply place the pizza in the microwave and let it cook. The result is a crunchy and delicious pizza that will satisfy your cravings in no time.";
+    public static final String PIZZA_8_TITLE = "Freddy Fazbear Circle Treat";
+    public static final String PIZZA_8_DESCRIPTION = "A unique and fun treat inspired by the famous Freddy Fazbear.";
+    public static final String PIZZA_8_RECIPE = "Heat your oven to 220C and cut a Freddy Fazbear suit into circles. Bake the circles until they are crispy and enjoy the delightful combination of flavors.";
+    public static final String PIZZA_9_TITLE = "Mega Slice Pizza";
+    public static final String PIZZA_9_DESCRIPTION = "Indulge in a single, massive slice of super mega pizza.";
+    public static final String PIZZA_9_RECIPE = "The recipe for this extraordinary pizza is classified. Only the chosen few can experience its incredible taste and size.";
+    public static final String PIZZA_10_TITLE = "Boxed Pizza Delight";
+    public static final String PIZZA_10_DESCRIPTION = "A challenging but rewarding pizza preparation that will delight your taste buds.";
+    public static final String PIZZA_10_RECIPE = "Start by acquiring a box. Then, either purchase a pizza or prepare one using our amazing recipes. Finally, bake the box with the pizza inside until it reaches a perfect crispy texture.";
+
+}
